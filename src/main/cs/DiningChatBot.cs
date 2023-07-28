a7cb3176f01089fe451eca06723302096d076bf3
Kofi <112496906+kamoah233@users.noreply.github.com>
2023-07-27 21:28:06 -0400

    Created DiningChatBot class

---
+++ src/main/cs/DiningChatBot.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class DiningChatBot
+{
+	public DiningChatBot()
+	{
+		int rating;
+		string restaurant;
+		string location;
+
+		Console.WriteLine("Please enter your location:");
+	}
+}
